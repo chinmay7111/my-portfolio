717efea963de7109db0e8feb93b7f1b7b8a1b696
chinmay7111 <chinmayingle62@gmail.com>
2023-12-04 10:13:01 +0530

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function Navbar() {
+  return (
+    <>
+     <nav className="navbar navbar-expand-lg bg-body-tertiary mb-100 ">
+  <div className="container-fluid">
+    <p className="navbar-brand mb-3 h1">Chinmay</p>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <p className="nav-link active" aria-current="page" >Education</p>
+        </li>
+        <li className="nav-item">
+          <p className="nav-link" href="#">Thechnologies</p>
+        </li>
+        <li className="nav-item">
+          <p className="nav-link" href="#">Project</p>
+        </li>
+        <li className="nav-item">
+          <p className="nav-link disabled" aria-disabled="true">Contact</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/Components/Technologies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.css.map"
+function Technologies() {
+  return (
+    <div className='container-fluid bg-dark text-'>
+      this is Technologies
+    </div>
+  )
+}
+
+export default Technologies
